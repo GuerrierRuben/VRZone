437ac14db7852cdd4c367576abf6b8c8ea94291a
GuerrierRuben <129087475+GuerrierRuben@users.noreply.github.com>
2024-01-31 01:12:25 -0500

    Delete JS directory

--- JS/app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    const scrolled = window.scrollY > 30
-
-    if (scrolled) {
-        document.body.classList.add('scrolled')
-    } else {
-        document.body.classList.remove('scrolled')
-    }
-})
